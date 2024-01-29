f87731ccf49caefbdc72f455af3f505d183a33cc
DaniAm94 <dani.amico94@gmail.com>
2024-01-29 15:17:49 +0100

    completed exercise

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 console.log(Vue)
 
+const endPoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const { createApp } = Vue;
 const app = createApp({
-    data: () => {
-        emailAddresses: []
+    data: () => ({
+        emailList: []
+    }),
+    methods: {
+        fetchEmail() {
+            axios.get(endPoint).then((res) => {
+                this.emailList.push(res.data.response);
+            })
+        }
+    },
+    created() {
+        for (let i = 1; i <= 10; i++)
+            this.fetchEmail();
     }
-});
+})
+
+
 
 app.mount('#root');
